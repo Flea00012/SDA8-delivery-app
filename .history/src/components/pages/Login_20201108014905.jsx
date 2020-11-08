68d40fe9e6f0ea23cdbb1db3a392e66723a4408d
Lee Fowler <fowler.lee8@gmail.com>
2020-11-08 01:58:02 +0100

    Updated the CSS styling and made the pages mobile first. Updated the sign up and login pages to reflect under-construction status

---
+++ .history/src/components/pages/Login_20201108014905.jsx
@@ -0,0 +1,32 @@
+//react core
+
+//import styles
+import image from '../../artwork/7028de95-3f1c-4817-ac8e-846941592c57_200x200.png';
+import '../../css/styles.css';
+
+export default function Login() {
+  return (
+    <>
+      <div className="packageStyle">
+        <div>
+          <h2> Welcome to Express Delivery </h2>
+          <p>
+            {' '}
+            If you already have a login name and password, please enter the site {' '}
+          </p>
+
+          <h1> Please note ourLogin Page is under development!!  </h1>
+        </div>
+        <div>
+          <img
+            className="picture"
+            src={image}
+            alt="a package is delivered to the client "
+          />
+          <p> Please enter your name and phone number to login</p>
+          <input placeholder="name" /> <input placeholder="phone number" />{' '}
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ .history/src/components/pages/SignUp_20201108013522.jsx
@@ -0,0 +1,29 @@
+import image from '../../artwork/7028de95-3f1c-4817-ac8e-846941592c57_200x200.png';
+import '../../css/styles.css';
+
+export default function Login() {
+  return (
+    <>
+      <div className="packageStyle">
+        <div>
+          <h2> Welcome to Express Delivery </h2>
+          <p>
+            {' '}
+            Please sign up for a free account and become our valued customer{' '}
+          </p>
+        </div>
+
+        <div>
+        <img
+          className="picture"
+          src={image}
+          alt="a package is delivered to the client "
+        />
+        <p> Please enter your name and phone number to login</p>
+        <input placeholder="name" /> <input placeholder="phone number" /> <></>
+        <input placeholder="address" />
+        <div></div>
+      </div>
+    </>
+  );
+}
